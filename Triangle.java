cdabbaa940e7d9d91bcead7b55baac0aa62771b8
grace-mao23 <gmao00@stuy.edu>
2018-10-14 16:45:10 -0400

    Outline

---
+++ Triangle.java
@@ -0,0 +1,27 @@
+public class Triangle{
+  private Point v1;
+  private Point v2;
+  private Point v3;
+
+  public Triangle(Point p, Point q, Point r){
+
+  }
+  public Triangle(double x1, double x2, double x3, double y1, double y2, double y3){
+
+  }
+
+  public double getPerimeter(){
+
+  }
+  public Point getVertex(int a){
+
+  }
+
+  public void setVertex(int index, Point newP){
+    
+  }
+
+  public String toString(){
+    return "Triangle: A" + v1 + " B" + v2 + " C" + v3;
+  }
+}
