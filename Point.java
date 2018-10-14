5afa2e904c06dc1d0fcf2c6444f394227d3bf343
grace-mao23 <gmao00@stuy.edu>
2018-10-14 16:52:47 -0400

    Filled in stuff

--- Point.java
+++ Point.java
@@ -2,28 +2,29 @@ public class Point{
   private double x;
   private double y;
 
-  // a constructor!
   public Point(double a, double b) {
     x = a;
     y = b;
   }
+
   public Point(Point p){
-    point = p;
+    x = p.x;
+    y = p.y;
   }
 
   public double getX(){
-    return x;
+    return this.x;
   }
   public double getY(){
-    return y;
+    return this.y;
   }
 
   public double distanceTo(Point p){
-
+    return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
   }
 
   public double distance(Point p, Point q){
-
+    return Math.sqrt(Math.pow(p.x - q.x, 2) + Math.pow(p.y - q.y, 2));
   }
 
   public String toString(){
